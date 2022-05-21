98095350e6f399d75c8e45e958c59d0b2fcb8654
Alan Doyle <alanduvgall@gmail.com>
2022-05-21 06:48:53 +0100

    add function to remove and add grid with prompt

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
+
+let gridSquare;
+
 const container = document.createElement('div');
 document.body.appendChild(container);
 container.classList.add('container');
 
 const gridContainer = document.createElement('div');
-container.appendChild(grid-container);
-grid-container.classList.add('grid-container');
+container.appendChild(gridContainer);
+gridContainer.classList.add('grid-container');
+
+const sliderContainer = document.createElement('div');
+container.appendChild(sliderContainer);
+sliderContainer.classList.add('slide-container');
+
+const slider = document.createElement('input');
+slider.type = 'range';
+slider.min = 4;
+slider.max = 100;
+slider.classList.add('slider');
+sliderContainer.appendChild(slider);
 
 let Add = document.createDocumentFragment();
 
 for (let i = 0; i < 16; i++) {
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('grid-square');
+let gridSquare = document.createElement('div');
+gridSquare.classList.add('grid-square');
+gridSquare.id = ('square ' + i);
+gridContainer.appendChild(gridSquare);
+};
+/*
+gridSquare.addEventListener("mouseenter", e => {
+  e.style.backgroundColor = 'black';
+}  )
+*/
+function removeGrid() {
+  while(gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.lastChild);
+  }
+  createGrid();
+}
+
+function createGrid() {
+  let input = prompt('Please enter a number between 4 and 100');
+
+  for(let i = 0; i < input; i++) {
+    let gridSquare = document.createElement('div');
+    gridSquare.classList.add('grid-square');
+    gridSquare.id = ('square ' + i);
+    gridContainer.appendChild(gridSquare);
+  }
 }
